8785ff655b2ec43b5976922eb7dfaaf83c3450e2
Joao-Guil <127537931+Joao-Guil@users.noreply.github.com>
2024-11-20 16:13:31 -0400

    continue

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-import express from "express";
+import express from 'express';
+import conectarAoBanco from './src/config/dbConfig';
+await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const posts = [
     { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
@@ -10,20 +12,20 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+  console.log("Servidor escutando...");
 });
 
 app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
+  res.status(200).json(posts);
 });
 
 function buscarPostPorID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
 }
 
 app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
+  const index = buscarPostPorID(req.params.id);
+  res.status(200).json(posts[index]);
 });
